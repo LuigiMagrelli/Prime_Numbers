47b6a566d6497e581cfab94eb26cec5ef9966e3b
luigi.magrelli@gmail.com <luigi.magrelli@gmail.com>
2019-10-14 20:47:27 -0700

    Initial Commit

---
+++ Prime_Numbers/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Prime_Numbers
+//
+//  Created by Luigi Magrelli on 10/14/19.
+//  Copyright © 2019 Luigi Magrelli. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
